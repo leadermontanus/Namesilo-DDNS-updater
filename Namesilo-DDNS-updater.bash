6e8cfa31ba316386cca4f88cd4ba530486549e12
Arild Bergh <arild.bergh@gmail.com>
2026-01-01 16:00:56 +0100

    Lots of bux fixes
    Using on Manjaro (current version Dec. 2025) there were lots of bugs in Namesilo-DDNS-updater.bash, in particular xmllint usage, order of certain commands, repeated calling API for same domain, etc.

--- Namesilo-DDNS-updater.bash
+++ Namesilo-DDNS-updater.bash
@@ -23,12 +23,12 @@ LOG_FILE=$(dirname "${BASH_SOURCE[0]}")/$LOG_FILE
 
 ## Fcts :
 namesilo_update() {
-  local ip_type=$1      # V4 or V6
-  local record_type=$2  # A or AAAA
-  local ttl=$3
+  ip_type=$1      # V4 or V6
+  record_type=$2  # A or AAAA
+  ttl=$3
 
   ## Retreive current external IP
-  local IP_ADDRESS=$( curl -s -${ip_type: -1} https://ifconfig.co/ip )
+  IP_ADDRESS=$( curl -s -${ip_type: -1} https://ifconfig.co/ip )
   if [[ -z "$IP_ADDRESS" || "$IP_ADDRESS" == *"error"* ]] || [[ "$ip_type" == "V6" && ! "$IP_ADDRESS" =~ ":" ]]; then
     [[ $VERBOSE ]] && printf "IP$ip_type Address can't be determined ($IP_ADDRESS)\n"
     update_line_or_add_one "Address can't be determined"
@@ -39,36 +39,45 @@ namesilo_update() {
     Current IP: '$IP_ADDRESS'\n"
 
   ## Set save files
-  local IP_FILE="./$DOMAIN/$ip_type-$DOMAIN-PubIP"
-  local IP_TIME="./$DOMAIN/$ip_type-$DOMAIN-IPTime"
+  IP_FILE="./$DOMAIN/$ip_type-$DOMAIN-PubIP"
+  IP_TIME="./$DOMAIN/$ip_type-$DOMAIN-IPTime"
+
   ## Retreive old IP
-  [[ -f $IP_FILE ]] && local KNOWN_IP=$(cat $IP_FILE) || local KNOWN_IP=
+  [[ -f $IP_FILE ]] && KNOWN_IP=$(cat $IP_FILE) || KNOWN_IP=
+
 
   [[ $VERBOSE ]] && printf "\
-      Known IP: '$KNOW_IP'\n\n"
+      Known IP: '$KNOWN_IP'\n\n"
 
   ## Check if IP has changed
   if [[ "$IP_ADDRESS" != "$KNOWN_IP" ]]; then
     [[ $VERBOSE ]] && printf "    IP differs from known !\n\n"
+
+    ## Retreive all records for the domain and store temporarily in file
+    curl -s "https://www.namesilo.com/api/dnsListRecords?version=1&type=xml&key=$APIKEY&domain=$DOMAIN" > $RECORDS
+
+    # loop through hosts to update
     for HOST in "${HOSTS[@]}"; do
       # @ is the bare domain.  Dots are not required, but handle correctly in
       # case the user puts them in anyway.
 
       ## remove @ and . from HOST
-      HOST=${HOST%@}
-      HOST=${HOST%.}
+      # XXXX REMOVED, caused @ which is valid prefix for DNS to be removed HOST=${HOST%@}
+      # XXXX REMOVED, no need HOST=${HOST%.}
+
       ## adding dot if there's an host
-      local HOST_DOT="${HOST:+$HOST.}"
-      ## Update current IP in file
+      HOST_DOT="${HOST:+$HOST.}"
+
+      ## Update current IP in file (it is reset further down, see: ## put the old IP back, so that the update will be tried next time
       echo $IP_ADDRESS > $IP_FILE
 
-      ## Retreive all records for the domain
-      curl -s "https://www.namesilo.com/api/dnsListRecords?version=1&type=xml&key=$APIKEY&domain=$DOMAIN" > $RECORDS
 
       ## Extract record ID for the domain, ip_type and host
-      local RECORD_ID=`xmllint --xpath "//namesilo/reply/resource_record/record_id[../host/text() = '$HOST_DOT$DOMAIN' ][../type = '$record_type' ]" $RECORDS 2>/dev/null`
+## OLDRECORD_ID=`xmllint --xpath "//namesilo/reply/resource_record/record_id[../host/text() = '$HOST_DOT$DOMAIN' ][../type = '$record_type' ]" $RECORDS 2>/dev/null`
+      RECORD_ID=`xmllint --xpath "//namesilo/reply/resource_record[type='A' and host='$HOST']/record_id" $RECORDS 2>/dev/null`
       RECORD_ID=${RECORD_ID#*>}
       RECORD_ID=${RECORD_ID%<*}
+
 
       #if RECORD_ID is empty, then the record does not exist, so create it
       if [[ -z "$RECORD_ID" ]]; then
@@ -76,11 +85,14 @@ namesilo_update() {
         curl -s "https://www.namesilo.com/api/dnsAddRecord?version=1&type=xml&key=$APIKEY&domain=$DOMAIN&rrtype=$record_type&rrhost=$HOST&rrvalue=$IP_ADDRESS&rrttl=$ttl" > $RESPONSE
       else
         [[ $VERBOSE ]] && printf "    Record found\n    Updating record for '$HOST_DOT$DOMAIN'\n"
-        [[ $VERBOSE ]] && printf "    IP on Namesilo: '$RECORD_VALUE'\n"
+
         ## Extract IP from Record
-        local RECORD_VALUE=`xmllint --xpath "//namesilo/reply/resource_record/value[../record_id/text() = '$RECORD_ID' ]" $RECORDS`
+# OLD   RECORD_VALUE=`xmllint --xpath "//namesilo/reply/resource_record/value[../record_id/text() = '$RECORD_ID' ]" $RECORDS`
+        RECORD_VALUE=`xmllint --recover --xpath "//namesilo/reply/resource_record/value[../record_id='$RECORD_ID']" $RECORDS`
         RECORD_VALUE=${RECORD_VALUE#*>}
         RECORD_VALUE=${RECORD_VALUE%<*}
+
+        [[ $VERBOSE ]] && printf "    IP on Namesilo: '$RECORD_VALUE'\n"
 
         ## IP is already on Namesilo (will be executed on first run if you've set up the right IP)
         if [[ "$RECORD_VALUE" == "$IP_ADDRESS" ]]; then
@@ -89,11 +101,11 @@ namesilo_update() {
           continue
         else
           ## Update record
-          curl -s "https://www.namesilo.com/api/dnsUpdateRecord?version=1&type=xml&key=$APIKEY&domain=$DOMAIN&rrid=$RECORD_ID&rrhost=$HOST&rrvalue=$IP_ADDRESS&rrttl=$ttl" > $RESPONSE
+          curl -s "https://www.namesilo.com/api/dnsUpdateRecord?version=1&type=xml&key=$APIKEY&domain=$DOMAIN&rrtype=$record_type&rrid=$RECORD_ID&rrhost=$HOST&rrvalue=$IP_ADDRESS&rrttl=$ttl" > $RESPONSE
         fi
       fi
       ## Extract response code
-      local RESPONSE_CODE=`xmllint --xpath "//namesilo/reply/code/text()" $RESPONSE`
+      RESPONSE_CODE=`xmllint --xpath "//namesilo/reply/code/text()" $RESPONSE`
       case $RESPONSE_CODE in
         300)
           ## Set update time
@@ -101,15 +113,15 @@ namesilo_update() {
           printf "IP$ip_type : $(date +"%d/%m/%y | %H:%M:%S") - Operation success. Now '$HOST_DOT$DOMAIN' IP address is '$IP_ADDRESS'\n" >> $LOG_FILE
           ;;
         280)
-          local ERROR_DETAILS=`xmllint --xpath "//namesilo/reply/detail/text()" $RESPONSE`
+          ERROR_DETAILS=`xmllint --xpath "//namesilo/reply/detail/text()" $RESPONSE`
           printf "IP$ip_type : $(date +"%d/%m/%y | %H:%M:%S") -There has been an error with the content of the request, see below\n'$ERROR_DETAILS'\n\n\n" >> $LOG_FILE
           ## put the old IP back, so that the update will be tried next time
           echo $KNOWN_IP > $IP_FILE
           ;;
         *)
+          printf "IP$ip_type : $(date +"%d/%m/%y | %H:%M:%S") - DDNS update failed code '$RESPONSE_CODE'!\n$RESPONSE" >> $LOG_FILE
           ## put the old IP back, so that the update will be tried next time
           echo $KNOWN_IP > $IP_FILE
-          printf "IP$ip_type : $(date +"%d/%m/%y | %H:%M:%S") - DDNS update failed code '$RESPONSE_CODE'!\n" >> $LOG_FILE
           ;;
       esac
     done
@@ -120,9 +132,9 @@ namesilo_update() {
 }
 
 update_line_or_add_one() {
-  local message=$1
+  message=$1
   ## grab the last line of the log file that contains "IP$ip_type"
-  local LAST_IP_CHANGE_LINE=$(grep "IP$ip_type" $LOG_FILE | tail -n 1)
+  LAST_IP_CHANGE_LINE=$(grep "IP$ip_type" $LOG_FILE | tail -n 1)
 
   ## if the last line contains a date of format "%m-%d-%y | %H:%M:%S"
   if [[ $LAST_IP_CHANGE_LINE =~ [0-9]{2}-[0-9]{2}-[0-9]{2} ]]; then
@@ -158,11 +170,13 @@ for DOMAIN in "${DOMAINS[@]}"; do
   RECORDS="./$DOMAIN/$DOMAIN.xml"
 
   [[ $VERBOSE ]] && printf "  ----  Updating $DOMAIN !  ----\n\n"
-  if [[ $UPDATE_IP_V4 ]]; then
+
+  if (( UPDATE_IP_V4 )); then
     [[ $VERBOSE ]] && printf "Updating IP V4 records\n\n"
     namesilo_update V4 A $IP_V4_TTL
   fi
-  if [[ $UPDATE_IP_V6 ]]; then
+
+  if (( UPDATE_IP_V6 )); then
     [[ $VERBOSE ]] && printf "\n\nUpdating IP V6 records\n\n"
     namesilo_update V6 AAAA $IP_V6_TTL
   fi
